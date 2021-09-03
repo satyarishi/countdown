ad5fa3087bf5a1fd9449186f642b8cb7fa8ef2f3
SATYA PRAKASH KUMAR <satya3753@gmail.com>
2021-09-03 19:48:36 +0530

    countdown function added in countdown.py

--- countdown.py
+++ countdown.py
@@ -1,13 +1,14 @@
 import time
-n = 10
-while n > 0:
-    print(n)
-    time.sleep(.5)
-    n -=1
-    continue
-print("Takeoff!")
-while  n < 10:
-    n +=1
-    print(n)
-    time.sleep(.5)
-print("launch successful!!!")        
+def countdown(n):
+    while n > 0:
+        print(n)
+        time.sleep(.5)
+        n -=1
+        continue
+    print("HERE WE GOOOOOOO!")
+    while  n < 10:
+        n +=1
+        print(n)
+        time.sleep(.5)
+    print("launch successful!!!")
+countdown(10)    
