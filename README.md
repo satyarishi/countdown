643956f30641a97ba18f56aa5804608550cccf72
SATYA PRAKASH KUMAR <satya3753@gmail.com>
2021-09-03 19:53:31 +0530

    modification in countdown.py and readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # countdown
+
+this program print the number after 1 second of interval, before and afeter the event happened.
--- countdown.py
+++ countdown.py
@@ -2,13 +2,13 @@ import time
 def countdown(n):
     while n > 0:
         print(n)
-        time.sleep(.5)
+        time.sleep(1)
         n -=1
         continue
     print("HERE WE GOOOOOOO!")
     while  n < 10:
         n +=1
         print(n)
-        time.sleep(.5)
+        time.sleep(1)
     print("launch successful!!!")
 countdown(10)    
